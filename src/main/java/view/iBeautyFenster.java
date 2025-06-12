b3593f978ee3101843e8a8b794c62210a838ce29
jbr.vy <ngothaovy@11012003gmail.com>
2025-06-12 14:56:15 +0200

    iBeautyFenster (GUI Form) bearbeitet

--- src/main/java/view/iBeautyFenster.java
+++ src/main/java/view/iBeautyFenster.java
@@ -8,21 +8,23 @@ public class iBeautyFenster extends JFrame{
     private JCheckBox chk_Dienst1;
     private JCheckBox chk_Dienst2;
     private JCheckBox chk_Dienst3;
-    private JComboBox cbx_Dienstleistung1;
-    private JComboBox cbx_Dienstleistung2;
-    private JComboBox cbx_Dienstleistung3;
+    private JComboBox cbx_Angebot1;
+    private JComboBox cbx_Angebot2;
+    private JComboBox cbx_Angebot3;
     private JTextField tf_Filtern;
-    private JRadioButton rdb_Filtern;
     private JPanel myPanel;
-    private JLabel lbl_slogan;
+    private JLabel lbl_Slogan;
     private JLabel lbl_Name;
     private JLabel lbl_Termin;
-    private JLabel lbl_Uhrzeit;
     private JLabel lbl_Telefon;
-    private JLabel lbl_Dienst;
-    private JLabel lbl_Dienstleistung;
+    private JLabel lbl_Dienste;
+    private JLabel lbl_Angeboten;
+    private JButton btn_Filtern;
     private JScrollPane scp_Termin_Uebersicht;
     private JTable tb_Termin_Uebersicht;
+    private JButton btn_Speichern_und_Anzeigen;
+    private JLabel lbl_Gesamtpreis;
+    private JTextField textField1;
 
     public iBeautyFenster() {
         setTitle("iBeauty Manager");
