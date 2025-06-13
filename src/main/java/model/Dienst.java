e8518fc4501c924f88692dfcc43cd843fcf9caf2
busrabekci <buesra.bekci@student.hnu.de>
2025-06-13 13:06:54 +0200

    Dienst Überarbeitung mit Kommentaren

--- src/main/java/model/Dienst.java
+++ src/main/java/model/Dienst.java
@@ -1,21 +1,51 @@
 package model;
 
-import javax.naming.Name;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// Die Klasse Dienst repräsentiert eine Dienstleistung mit Namen, Kategorie (Angebot) und Preis
 public class Dienst {
-    private String name;
+    // Attribute zur Speicherung von Namen des Dienstes, Angebot und Preis
+    private String dienst;
+    private String angebot;
     private double preis;
 
-    public Dienst (String name, double preis) {
-        this.name = name;
+    // Konstruktor, um ein Termin-Objekt mit Werten zu erstellen
+    public Dienst (String dienst, String angebot, double preis) {
+        this.dienst = dienst;
+        this.angebot = angebot;
         this.preis = preis;
     }
 
-    public String getName (){
-        return name;
+    // Getter-Methode für den Dienstnamen
+    public String getDienst (){
+        return dienst;
     }
 
+    // Setter-Methode für den Dienstnamen
+    public void setDienst (String dienst) {
+        this.dienst = dienst;
+    }
+
+    // Getter-Methode für das Angebot
+    public String getAngebot (){
+        return angebot;
+    }
+
+    // Setter-Methode für das Angebot (Kategorie)
+    public void setAngebot (String Angebot) {
+        this.angebot = angebot;
+    }
+
+    // Getter-Methode für den Preis
     public double getPreis(){
         return preis;
     }
+
+    // Setter-Methode für den Preis
+    public void setPreis (double preis) {
+        this.preis = preis;
+    }
+
 }
