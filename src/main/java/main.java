d95a7e2ad4b7014f1389337a5333e4e09471f2a2
jbr.vy <ngothaovy@11012003gmail.com>
2025-06-22 14:49:22 +0200

    Main class hinzufügt

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,19 @@
-public class main {
+import controller.ViewController;
+import view.iBeautyFenster;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                iBeautyFenster view = new iBeautyFenster();
+                ViewController controller = new ViewController(view);
+                controller.iBeautyFenster_anzeigen();
+            }
+        });
+    }
+
 }
